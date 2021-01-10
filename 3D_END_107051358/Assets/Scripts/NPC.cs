62e42814db48c408be77df2e0f3fb4de8ed4acb4
107051358 <72062978+107051358@users.noreply.github.com>
2021-01-11 02:52:55 +0800

    想上廁所趕快存一下

--- 3D_END_107051358/Assets/Scripts/NPC.cs
+++ 3D_END_107051358/Assets/Scripts/NPC.cs
@@ -1,23 +1,76 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class NPC : MonoBehaviour
 {
     [Header("NPC資料")]
-    public NPCData date;
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialogue;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.2f;
 
     public bool playerInArea;
+
+    public enum NPCState
+    {
+        FirstDialogue,Missing,Finish
+    }
+    public NPCState state = NPCState.FirstDialogue;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name == "小明")
+        if (other.name == "老公公")
         {
             playerInArea = true;
+            StartCoroutine(Dialogue());
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.name == "小明")
+        if (other.name == "老公公")
         {
             playerInArea = false;
+            StopDialogue();
         }
+    }
+    private void StopDialogue()
+    {
+        dialogue.SetActive(false);
+        StopAllCoroutines();
+    }
+
+    private IEnumerator Dialogue()
+    {
+        dialogue.SetActive(true);
+        textContent.text = "";
+        textName.text = name;
+
+        string dialogueString = data.dialogueB;
+
+        switch (state)
+        {
+            case NPCState.FirstDialogue:
+                dialogueString = data.dialogueA;
+                break;
+            case NPCState.Missing:
+                dialogueString = data.dialogueB;
+                break;
+            case NPCState.Finish:
+                dialogueString = data.dialogueC;
+                break;
+        }
+
+        for (int i = 0; i < dialogueString.Length; i++)
+        {
+            textContent.text += dialogueString[i] + "";
+            yield return new WaitForSeconds(interval);
+        } 
+      
     }
 }
