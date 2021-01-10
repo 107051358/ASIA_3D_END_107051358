a9dc9b67ce4f05c47830139600a70b3b286cc97f
107051358 <72062978+107051358@users.noreply.github.com>
2021-01-10 21:55:07 +0800

    npc偵測玩家結束要開會了

---
+++ 3D_END_107051358/Assets/Scripts/NPC.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData date;
+
+    public bool playerInArea;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "小明")
+        {
+            playerInArea = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "小明")
+        {
+            playerInArea = false;
+        }
+    }
+}
