9aed12cad8968ef31c85033ffffaaeee92c94f16
107051358 <72062978+107051358@users.noreply.github.com>
2021-01-13 12:08:36 +0800

    下次從玩家的連擊動畫控制速度等於3開始

--- 3D_END_107051358/Assets/Scripts/Enemy.cs
+++ 3D_END_107051358/Assets/Scripts/Enemy.cs
@@ -9,6 +9,12 @@ public class Enemy : MonoBehaviour
     public float stopDistence = 2.5f;
     [Header("攻擊冷卻時間"), Range(0, 50)]
     public float cd = 2f;
+    [Header("攻擊中心點")]
+    public Transform atkPoint;
+    [Header("攻擊長度"), Range(0f, 5f)]
+    public float atkLength;
+    [Header("攻擊力"), Range(0, 500)]
+    public float atk = 30;
 
     private Transform player;
     private NavMeshAgent nav;
@@ -29,6 +35,12 @@ public class Enemy : MonoBehaviour
         Track();
         Attack();
     }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(atkPoint.position, atkPoint.forward * atkLength);
+    }
+    private RaycastHit hit;
     private void Attack()
     {
         if (nav.remainingDistance < stopDistence)
@@ -40,6 +52,11 @@ public class Enemy : MonoBehaviour
             {
                 ani.SetTrigger("攻擊觸發");
                 timer = 0;
+                if (Physics.Raycast(atkPoint.position, atkPoint.forward, out hit, atkLength, 1 << 8))
+                {
+                    hit.collider.GetComponent<Player>().Damage(atk);
+                }
+                
             }
             
         }
---
+++ 3D_END_107051358/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using Invector.vCharacterController;
+
+public class Player : MonoBehaviour
+{
+    private float hp = 100;
+    private Animator ani;
+    private int atkCount;
+    private float timer;
+    [Header("連擊間隔時間"),Range(0, 3)]
+    public float interval = 1;
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        Attack();
+    }
+    private  void Attack()
+    {
+        if (atkCount < 3)
+        {
+            if (timer < interval)
+            {
+                timer += Time.deltaTime;
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    atkCount++;
+                    timer = 0;
+                    ani.SetInteger("連擊", atkCount);
+                }
+            }
+            else
+            {
+                atkCount = 0;
+                timer = 0;
+            }
+        }
+        if (atkCount == 3) atkCount = 0;
+        ani.SetInteger("連擊",atkCount);
+    }
+    public  void Damage(float damage)
+    {
+        hp -= damage;
+        ani.SetTrigger("受傷觸發");
+        if (hp <= 0) Dead();
+    }
+    private void Dead()
+    {
+        ani.SetTrigger("死亡觸發");
+        vThirdPersonController vt = GetComponent<vThirdPersonController>();
+        vt.lockMovement = true;
+        vt.lockRotation = true;
+    }
+}
